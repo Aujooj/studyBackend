d32320ab9be9e95a0c06ed17c49775635b2316f0
João Augusto de Souza Santos <a61468@alunos.ipb.pt>
2024-11-23 01:56:19 +0000

    deploy

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
-gcloud services enable run.googleapis.com --project
-gcloud services enable cloudbuild.googleapis.com --project
-gcloud services enable artifactregistry.googleapis.com --project
+gcloud config set project superb-avatar-442601-k7
+gcloud services enable run.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
